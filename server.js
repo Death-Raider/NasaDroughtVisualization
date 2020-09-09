a45c26dfb9d432ed3516c552000f1f30ee53ee80
unknown <oragimirox@gmail.com>
2020-09-10 01:36:29 +0530

    working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //require
 const express = require('express');
-const spawn = require('child_process').spawn;
+const fs = require('fs');
+
 
 //uses the files present in folder public
 const app = express();
@@ -15,37 +16,13 @@ app.use(
 );
 
 app.post('/apiPost', async (request,responce) => {
+  const dataGot = request.body;
+  const dataRaw = JSON.parse(fs.readFileSync(`newData/${dataGot.val}.txt`).toString("utf8"));
+  responce.send(JSON.stringify(dataRaw))
 });
 
-//to get the prediction from the NN that was called
 app.get('/api',async (request, responce) => {
-  const datagot = request.body;//gets the content submitted
-  const droughtData10 = new Promise( (resolve,reject) => {
-    callFile('test.js',resolve,reject)
-  });
-  let coordinates = await droughtData10;
-  coordinates = coordinates.split("\n")
-  coordinates.pop()
-  console.log(coordinates.map(JSON.parse));
-  console.log(coordinates.length);
-  responce.send(JSON.stringify(coordinates));
+
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
-
-function callFile(filePath,resolve,reject){
-  const test = spawn('node',[filePath]);
-  let x="";
-
-  test.stdout.on('data',(data) =>{
-    x += data.toString('utf8')+" ";
-  });
-  test.stdout.on('end',()=>{
-    resolve(x)
-  });
-  test.stderr.on('error',(err)=>{
-    console.log(err);
-    reject(err)
-  });
-
-}
